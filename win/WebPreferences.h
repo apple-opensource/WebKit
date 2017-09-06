60407285f5cf6e0dd762b2a2c1637cf535b6125c
Apple Open Source <apple.opensource@gmail.com>
2017-09-06 17:42:59 +0000

    7603.2.4

--- win/WebPreferences.h
+++ win/WebPreferences.h
@@ -23,15 +23,14 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#ifndef WebPreferences_H
-#define WebPreferences_H
+#pragma once
 
 #include "WebKit.h"
 #include <CoreFoundation/CoreFoundation.h>
 #include <WebCore/BString.h>
 #include <wtf/RetainPtr.h>
 
-class WebPreferences : public IWebPreferences, public IWebPreferencesPrivate3 {
+class WebPreferences : public IWebPreferences, public IWebPreferencesPrivate4 {
 public:
     static WebPreferences* createInstance();
 protected:
@@ -235,12 +234,19 @@ public:
     virtual HRESULT STDMETHODCALLTYPE setShowTiledScrollingIndicator(BOOL);
     virtual HRESULT STDMETHODCALLTYPE fetchAPIEnabled(_Out_ BOOL*);
     virtual HRESULT STDMETHODCALLTYPE setFetchAPIEnabled(BOOL);
-    virtual HRESULT STDMETHODCALLTYPE domIteratorEnabled(_Out_ BOOL*);
-    virtual HRESULT STDMETHODCALLTYPE setDOMIteratorEnabled(BOOL);
     virtual HRESULT STDMETHODCALLTYPE shadowDOMEnabled(_Out_ BOOL*);
     virtual HRESULT STDMETHODCALLTYPE setShadowDOMEnabled(BOOL);
     virtual HRESULT STDMETHODCALLTYPE customElementsEnabled(_Out_ BOOL*);
     virtual HRESULT STDMETHODCALLTYPE setCustomElementsEnabled(BOOL);
+    virtual HRESULT STDMETHODCALLTYPE modernMediaControlsEnabled(_Out_ BOOL*);
+    virtual HRESULT STDMETHODCALLTYPE setModernMediaControlsEnabled(BOOL);
+    virtual HRESULT STDMETHODCALLTYPE es6ModulesEnabled(_Out_ BOOL*);
+    virtual HRESULT STDMETHODCALLTYPE setES6ModulesEnabled(BOOL);
+
+    // IWebPreferencesPrivate4
+    virtual HRESULT STDMETHODCALLTYPE setApplicationId(BSTR);
+    virtual HRESULT STDMETHODCALLTYPE allowsPageCacheWithWindowOpener(_Out_ BOOL*);
+    virtual HRESULT STDMETHODCALLTYPE setAllowsPageCacheWithWindowOpener(BOOL);
 
     // WebPreferences
 
@@ -255,14 +261,16 @@ public:
     static void removeReferenceForIdentifier(BSTR identifier);
     static WebPreferences* sharedStandardPreferences();
 
+    static CFStringRef applicationId();
+
     // From WebHistory.h
     HRESULT historyItemLimit(_Out_ int*);
     HRESULT setHistoryItemLimit(int);
     HRESULT historyAgeInDaysLimit(_Out_ int*);
     HRESULT setHistoryAgeInDaysLimit(int);
 
-     void willAddToWebView();
-     void didRemoveFromWebView();
+    void willAddToWebView();
+    void didRemoveFromWebView();
 
     HRESULT postPreferencesChangesNotification();
 
@@ -295,6 +303,5 @@ protected:
     bool m_autoSaves { false };
     bool m_automaticallyDetectsCacheModel { true };
     unsigned m_numWebViews { 0 };
+    static RetainPtr<CFStringRef> m_applicationId;
 };
-
-#endif
